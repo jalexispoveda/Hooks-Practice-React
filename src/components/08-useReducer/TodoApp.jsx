9b6e2328be2fe652f56f4c384519cd55bb8bc47e
jalexispoveda010 <jalexispoveda010@gmail.com>
2022-09-10 06:31:11 -0500

    Reducer implementacion de useEffect para guardar en local storage, implementacion de init

--- src/components/08-useReducer/TodoApp.jsx
+++ src/components/08-useReducer/TodoApp.jsx
@@ -1,48 +1,48 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
 import { todoReducer } from "./todoReducer";
 
 //Objeto inicial
-const initialState = [
-	{
-		id: new Date().getTime(),
-		desc: "Recolectar la piedra del alma",
-		done: false,
-	},
-	{
-		id: new Date().getTime() * 3,
-		desc: "Meditar",
-		done: false,
-	},
-];
+const initialState = [];
 
+//funcion para inicializar el reducer
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos") || []);
+};
 export const TodoApp = () => {
-	//Si tengo mas de un reducer en el mismo functional component especifico nombre en el dispatch
-	const [todos, dispatchTodos] = useReducer(todoReducer, initialState);
+  //Si tengo mas de un reducer en el mismo functional component especifico nombre en el dispatch
+  const [todos, dispatchTodos] = useReducer(todoReducer, initialState, init);
 
-	const handleNewTodo = todo => {
-		dispatchTodos({
-			type: "add",
-			payload: todo,
-		});
-	};
-	return (
-		<>
-			<h1>
-				Todo App: ({todos.length}) <small>Pendientes: {todos.length}</small>
-			</h1>
-			<hr></hr>
-			<div className="row">
-				<div className="col-7">
-					<TodoList todos={todos} reducer={dispatchTodos} />
-				</div>
-				<div className="col-5">
-					<h4>Agregar TODO</h4>
-					<hr></hr>
-					<TodoAdd addTodo={handleNewTodo}></TodoAdd>
-				</div>
-			</div>
-		</>
-	);
+  useEffect(() => {
+    console.log(todos);
+    localStorage.setItem("todos", JSON.stringify(todos) || []);
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+
+    dispatchTodos(action);
+  };
+  return (
+    <>
+      <h1>
+        Todo App: ({todos.length}) <small>Pendientes: {todos.length}</small>
+      </h1>
+      <hr></hr>
+      <div className="row">
+        <div className="col-7">
+          <TodoList todos={todos} reducer={dispatchTodos} />
+        </div>
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr></hr>
+          <TodoAdd addTodo={handleNewTodo}></TodoAdd>
+        </div>
+      </div>
+    </>
+  );
 };
--- src/components/08-useReducer/todoReducer.js
+++ src/components/08-useReducer/todoReducer.js
@@ -1,18 +1,16 @@
 export const todoReducer = (state, action) => {
-	switch (action.type) {
-		case "ABC":
-			throw new Error("ABC action no esta implementada");
-		case "add":
-			return [...state, action.payload];
-		case "remove":
-			console.log(action);
-			return [
-				...state.slice(
-					state.indexOf(e => e.id === action.payload),
-					1
-				),
-			];
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case "[TODO] Add Todo":
+      return [...state, action.payload];
+    case "remove":
+      console.log(action);
+      return [
+        ...state.slice(
+          state.indexOf((e) => e.id === action.payload),
+          1
+        ),
+      ];
+    default:
+      return state;
+  }
 };
