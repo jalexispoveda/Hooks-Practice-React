f2c657f080acb07e3e8f5a6b9d93508230fe33bc
JorgeP <jalexispoveda010@gmail.com>
2022-09-12 20:39:21 -0500

    Agregado NavLink para saber en que ruta y estamos y dar estilo de bootstrap active

--- src/components/09-useContext/NavBar.jsx
+++ src/components/09-useContext/NavBar.jsx
@@ -1,12 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/login">Login</Link>
-    </>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        useContext
+      </Link>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink
+              to="/"
+              className={`nav-link ${(props) =>
+                props.isActive ? "active" : ""}`}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={`nav-link ${(props) =>
+                props.isActive ? "active" : ""}`}
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/login"
+              className={`nav-link ${({ isActive }) =>
+                isActive ? "active" : ""}`}
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
