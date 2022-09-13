fc98f3922fbbe6e588f9376e32254322801a8d30
JorgeP <jalexispoveda010@gmail.com>
2022-09-12 20:16:51 -0500

    Implementacion de link para navbar y redireccion react-router

--- src/components/09-useContext/MainApp.jsx
+++ src/components/09-useContext/MainApp.jsx
@@ -3,12 +3,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { AboutPage } from "./AboutPage";
 import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
+import { NavBar } from "./NavBar";
 // import { AboutPage, HomePage, LoginPage } from "./";
 
 export const MainApp = () => {
   return (
     <>
       <h1>Main App</h1>
+      <NavBar></NavBar>
       <hr></hr>
       <Routes>
         <Route path="/" element={<HomePage />} />
